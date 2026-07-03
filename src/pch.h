a49ba4ae26ffe1ff2e13d6a25eeb9d8a48a969d5
hokecche <35355836+hokecche@users.noreply.github.com>
2026-07-04 02:58:25 +0800

    Update pch.h

--- src/pch.h
+++ src/pch.h
@@ -4,11 +4,6 @@
 #include <RE/Fallout.h>
 #include <F4SE/F4SE.h>
 
-// 补全你之前用到的UI事件类的专属头文件，彻底解决找不到MenuOpenEvent/MenuCloseEvent的报错
-#include <RE/Menu/MenuOpenEvent.h>
-#include <RE/Menu/MenuCloseEvent.h>
-#include <RE/UI/HUDRenderEvent.h>
-
 // 按需引入标准库头文件，避免冗余引入拖慢编译速度
 #include <string>
 #include <format>
